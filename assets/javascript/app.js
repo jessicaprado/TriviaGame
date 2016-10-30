366f66bc7468b0aacf71cb5a575ce1c8b87624e8
Jessica Prado <jessica.marie.prado@gmail.comgit config --global user.name Jessica>
2016-10-30 11:29:25 -0400

    Able to tell if answer is true or false

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
-
+var answers = {
+	one: "Number 4 Privet Drive!",
+	two: "Dobby",
+};
 //time variables
 var second = 20;
 var counter = setInterval(timer, 1000);
+//questions variables
+var correct = 0;
+var incorrect = 0; 
+
+
+$(".buttons").on("click", function(){
+ 	var userGuess = $(this);
+	var checkGuess = userGuess.data("value");
+
+	if (checkGuess == true) {
+		correct++;
+		$(".content").html("<p>You are correct!</p>"); 
+    } else if (checkGuess == false) {
+    	incorrect++
+        $(".content").html("<p>Sorry! The correct answer was " + answers.one + "!</p>"); 
+    };	
+    });
+
 
 function timer () {
 	second -=1
@@ -11,8 +32,6 @@ function timer () {
 	}
 //display timer on HTML
 $("#timer").html("Time Left: " + second + " seconds");
-
-}
-
+};//timer function
 
 }); //END OF SCRIPT. DO NOT TOUCH
