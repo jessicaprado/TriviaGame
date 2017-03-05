575f01ac626b7437d724a19477839638e68469f3
Jessica Prado <jessica.marie.prado@gmail.com>
2017-03-04 22:59:50 -0500

    Trivia Game readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# TriviaGame
+# Trivia Game
+
+#### This app uses jquery and setTimeOut to deliver a timed quiz. Quiz questions are ordered in an array of objects which are looped through and delivered on the main content div.
+
+### Correct Answers
+
+#### When a question is answered correctly, the window changes and a point is added to your score
+
+<div align="center">
+<img src="assets/images/correct.gif" alt="correct" width="600">
+</div>
+
+### Incorrect Answers
+
+#### When a question is answered incorrectly, the app will inform you that you are incorrect and give you the correct answer. No points are added to your score.
+
+#### If the question isn't answered in the duration of the SetTimeOut, the question is counted as an incorrect answer.
+
+<div align="center">
+<img src="assets/images/incorrect.gif" alt="incorrect" width="600">
+</div>
+
+### Game Over and Reset
+
+#### When you have passed through all questions, you are informed of your final score. After a setTimeOut of 10 seconds, the game will begin again. You can bypass the setTimeOut by clicking the reset button.
+
+<div align="center">
+<img src="assets/images/reset.gif" alt="reset" width="600">
+</div>
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,191 +1,191 @@
 $(document).ready(function() {
-var second = 20;
-var counter = setInterval(timer, 1000);
-//questions variables
-var correct = 0;
-var incorrect = 0; 
-var unanswered = 0;
-var endOfQuiz = false;
-var index = 0;
+	var second = 20;
+	var counter = setInterval(timer, 1000);
+	//questions variables
+	var correct = 0;
+	var incorrect = 0; 
+	var unanswered = 0;
+	var endOfQuiz = false;
+	var index = 0;
 
-var quizQuestion1 = {
-	question: "Where does Harry live?",
-	options: ["Number 5 Privet Drive", "Number 4 Privet Drive", "Number 7 Privet Drive", "Number 3 Privet Drive"],
-	trueOrFalse: [false, true, false, false],
-	answer: "Number 4 Privet Drive"
-	}; 
+	var quizQuestion1 = {
+		question: "Where does Harry live?",
+		options: ["Number 5 Privet Drive", "Number 4 Privet Drive", "Number 7 Privet Drive", "Number 3 Privet Drive"],
+		trueOrFalse: [false, true, false, false],
+		answer: "Number 4 Privet Drive"
+		}; 
 
-var quizQuestion2 = {
-	question: "How many points is a Snitch worth?",
-	options: ["100", "450", "150", "300"],
-	trueOrFalse: [false, false, true, false],
-	answer: "150"
-	};
-
-var quizQuestion3 = {
-	question: "What is the Diadem of Ravenclaw ?",
-	options: ["An earring", "A necklace", "A ring", "A tiara"],
-	trueOrFalse: [false, false, false, true],
-	answer: "A tiara"
-	};
-
-var quizQuestion4 = {
-	question: "How many kids are in the Weasley family?",
-	options: ["7 kids", "8 kids", "6 kids", "5 kids"],
-	trueOrFalse: [true, false, false, false],
-	answer: "7 kids"
-	};
-
-var quizQuestion5 = {
-	question: "What did Dumbledore leave Ron in his will?",
-	options: ["A golden snitch", "The sword of Gryffindor", "A Deluminator", "The Tales of Beedle the Bard"],
-	trueOrFalse: [false, false, true, false],
-	answer: "A Deluminator"
-	};
-
-var quizQuestion6 = {
-	question: "What was James Potter's school nickname?",
-	options: ["Moony", "Wormtail", "Prongs", "Padfoot"],
-	trueOrFalse: [false, false, true, false],
-	answer: "Prongs"
-	};
-
-var quizQuestion7 = {
-	question: "When is Harry's Birthday?",
-	options: ["July 31st", "July 20th", "June 30th", "August 5th"],
-	trueOrFalse: [true, false, false, false],
-	answer: "July 31st"
-	};
-
-var quiz = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5, quizQuestion6, quizQuestion7];
-
-function reset () {
-	clear();
-	index = 0;
-	correct = 0;
-	incorrect = 0; 
-	unanswered = 0;
-	endOfQuiz = false;
-	second = 20;
-	counter = setInterval(timer, 1000);	
-	displayQuestion();
-	}; //reset end
-
-function displayQuestion() {
-	timer();
-	if(endOfQuiz === false) {
-	var questionhtml = quiz[index].question;
-	var questionPlacement = $(".question").html(questionhtml);
-	
-	//puts questions into text.
-	$(questionPlacement).html(questionhtml);
-
-	//loop to display questions on HTML
-	for (var i = 0; i < 4; i++) {
-		var guess = $("<button>");	
-		guess.addClass("selection");
-		guess.attr({
-			"data-options": quiz[index].trueOrFalse[i]	
-			});
-		guess.text(quiz[index].options[i]);
-		$(".options").append(guess);	
+	var quizQuestion2 = {
+		question: "How many points is a Snitch worth?",
+		options: ["100", "450", "150", "300"],
+		trueOrFalse: [false, false, true, false],
+		answer: "150"
 		};
 
-	check();
-	}}; //displayQuestion end
+	var quizQuestion3 = {
+		question: "What is the Diadem of Ravenclaw ?",
+		options: ["An earring", "A necklace", "A ring", "A tiara"],
+		trueOrFalse: [false, false, false, true],
+		answer: "A tiara"
+		};
 
-function check (){
-	$("button").on("click", function(){	
-	stopTimer();
-	var userGuess = $(this);
-	var checkGuess = userGuess.data("options");	
+	var quizQuestion4 = {
+		question: "How many kids are in the Weasley family?",
+		options: ["7 kids", "8 kids", "6 kids", "5 kids"],
+		trueOrFalse: [true, false, false, false],
+		answer: "7 kids"
+		};
 
-	if ((checkGuess === true) && (endOfQuiz === false)) {
-		right();
-	} else if((checkGuess === false) && (endOfQuiz === false)) {	
-		wrong();			
-	} 
- 	});
-	};
+	var quizQuestion5 = {
+		question: "What did Dumbledore leave Ron in his will?",
+		options: ["A golden snitch", "The sword of Gryffindor", "A Deluminator", "The Tales of Beedle the Bard"],
+		trueOrFalse: [false, false, true, false],
+		answer: "A Deluminator"
+		};
 
-function right() {
-	correct++
-	$(".result").show();	
-	$("<p id='correct'>Correct!</p>").appendTo(".result");
-	$(".selection").hide();
-	$(".question").hide();
-	index++;
-	setTimeout(nextQuestion, 3000);		
-	}
+	var quizQuestion6 = {
+		question: "What was James Potter's school nickname?",
+		options: ["Moony", "Wormtail", "Prongs", "Padfoot"],
+		trueOrFalse: [false, false, true, false],
+		answer: "Prongs"
+		};
 
-function wrong() {
-	var rightAnswer = quiz[index].answer;
-	incorrect++;	
-	$(".result").show();
-	$("<p id='nope'>Nope! The correct answer was " + rightAnswer + "</p>").appendTo(".result");	
-	$(".selection").hide();
-	$(".question").hide();
-	index++;
-	setTimeout(nextQuestion, 3000);	
-	}
+	var quizQuestion7 = {
+		question: "When is Harry's Birthday?",
+		options: ["July 31st", "July 20th", "June 30th", "August 5th"],
+		trueOrFalse: [true, false, false, false],
+		answer: "July 31st"
+		};
 
-function clear() {
-	$(".question").empty();
-	$(".options").empty();
-	$(".selection").empty();
-	$(".question").show();
-	$(".options").show();
-	$(".selection").show();
-	$(".result").empty();
-	}
+	var quiz = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5, quizQuestion6, quizQuestion7];
 
-function nextQuestion () {	
-	if (index > (quiz.length - 1)) {
-	endOfQuiz = true;
-	setTimeout(displayScore, 3000);
-	} else if (endOfQuiz === false) {
-	clear()	
-	resetTimer();
-	displayQuestion();
-	$(".result").hide();
-	}};
+	function reset () {
+		clear();
+		index = 0;
+		correct = 0;
+		incorrect = 0; 
+		unanswered = 0;
+		endOfQuiz = false;
+		second = 20;
+		counter = setInterval(timer, 1000);	
+		displayQuestion();
+		}; //reset end
 
-function displayScore() {
-	stopTimer()
-	$("#nope").hide();
-	$("#correct").hide();
-	$(".result").show();	
-	$("<p>Final Score</p><p>Correct: " + correct + "<p>Incorrect: " + incorrect + "</p><p>Unanswered:" + unanswered + "</p>").appendTo(".result");
-	$("<button class = 'reset'> Reset </button>").appendTo(".result");
-	$(".reset").on("click", function() {
-		reset();
-	});
-	}
+	function displayQuestion() {
+		timer();
+		if(endOfQuiz === false) {
+		var questionhtml = quiz[index].question;
+		var questionPlacement = $(".question").html(questionhtml);
+		
+		//puts questions into text.
+		$(questionPlacement).html(questionhtml);
 
-function timer () {
-	second -=1
-	if (second == 0){
+		//loop to display questions on HTML
+		for (var i = 0; i < 4; i++) {
+			var guess = $("<button>");	
+			guess.addClass("selection");
+			guess.attr({
+				"data-options": quiz[index].trueOrFalse[i]	
+				});
+			guess.text(quiz[index].options[i]);
+			$(".options").append(guess);	
+			};
+
+		check();
+		}}; //displayQuestion end
+
+	function check (){
+		$("button").on("click", function(){	
+		stopTimer();
+		var userGuess = $(this);
+		var checkGuess = userGuess.data("options");	
+
+		if ((checkGuess === true) && (endOfQuiz === false)) {
+			right();
+		} else if((checkGuess === false) && (endOfQuiz === false)) {	
+			wrong();			
+		} 
+	 	});
+		};
+
+	function right() {
+		correct++
+		$(".result").show();	
+		$("<p id='correct'>Correct!</p>").appendTo(".result");
+		$(".selection").hide();
+		$(".question").hide();
+		index++;
+		setTimeout(nextQuestion, 3000);		
+		}
+
+	function wrong() {
+		var rightAnswer = quiz[index].answer;
+		incorrect++;	
+		$(".result").show();
+		$("<p id='nope'>Nope! The correct answer was " + rightAnswer + "</p>").appendTo(".result");	
+		$(".selection").hide();
+		$(".question").hide();
+		index++;
+		setTimeout(nextQuestion, 3000);	
+		}
+
+	function clear() {
+		$(".question").empty();
+		$(".options").empty();
+		$(".selection").empty();
+		$(".question").show();
+		$(".options").show();
+		$(".selection").show();
+		$(".result").empty();
+		}
+
+	function nextQuestion () {	
+		if (index > (quiz.length - 1)) {
+		endOfQuiz = true;
+		setTimeout(displayScore, 3000);
+		} else if (endOfQuiz === false) {
+		clear()	
+		resetTimer();
+		displayQuestion();
+		$(".result").hide();
+		}};
+
+	function displayScore() {
+		stopTimer()
+		$("#nope").hide();
+		$("#correct").hide();
+		$(".result").show();	
+		$("<p>Final Score</p><p>Correct: " + correct + "<p>Incorrect: " + incorrect + "</p><p>Unanswered:" + unanswered + "</p>").appendTo(".result");
+		$("<button class = 'reset'> Reset </button>").appendTo(".result");
+		$(".reset").on("click", function() {
+			reset();
+		});
+		}
+
+	function timer () {
+		second -=1
+		if (second == 0){
+			clearInterval(counter);
+			unanswered++;
+			wrong();
+
+		}
+		//display timer on HTML
+		$("#timer").html("Time Left: " + second + " seconds");
+		};//timer function
+
+	function resetTimer() {
+		second = 20;	
+		counter = setInterval(timer, 1000);
+		//display timer on HTML
+		$("#timer").html("Time Left: " + second + " seconds");
+		}
+
+	function stopTimer() {
 		clearInterval(counter);
-		unanswered++;
-		wrong();
+		}
 
-	}
-	//display timer on HTML
-	$("#timer").html("Time Left: " + second + " seconds");
-	};//timer function
-
-function resetTimer() {
-	second = 20;	
-	counter = setInterval(timer, 1000);
-	//display timer on HTML
-	$("#timer").html("Time Left: " + second + " seconds");
-	}
-
-function stopTimer() {
-	clearInterval(counter);
-	}
-
-// START GAME
-displayQuestion();
+	// START GAME
+	displayQuestion();
 
 }); //END OF SCRIPT. DO NOT TOUCH
